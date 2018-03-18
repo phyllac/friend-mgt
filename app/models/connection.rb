7fa7dd09efee1111831c1b9204fde33e66579216
Phil Lacerna <phyllac@yahoo.com>
2018-03-19 00:59:29 +0800

    additional model validation

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,5 +1,26 @@
 class Connection < ApplicationRecord
     validates :requestor, presence: true
-    validates :target, presence: true        
+    validates :target, presence: true
     validates_uniqueness_of :target, scope: :requestor, message: 'Connection already exists'
+
+    validates_email_format_of :requestor, :message => 'is not a valid email address'
+    validates_email_format_of :target, :message => 'is not a valid email address'
+
+    validate :target_should_not_be_equal_with_requestor
+    validate :blocked_cant_be_added_as_friend
+    validate :blocked_requestor_cant_be_added_as_friend
+    
+    def target_should_not_be_equal_with_requestor
+        errors.add(:target, 'Email should not be the same as user') if target == requestor
+    end
+
+    def blocked_cant_be_added_as_friend
+        blocked = Block.where(:requestor => requestor, :target => target)
+        errors.add(:target, 'You have blocked this email') if blocked.count > 0
+    end
+
+    def blocked_requestor_cant_be_added_as_friend
+        blocked = Block.where(:requestor => target, :target => requestor)
+        errors.add(:target, 'You are blocked by the user') if blocked.count > 0
+    end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,13 @@ class Subscription < ApplicationRecord
     validates :requestor, presence: true
     validates :target, presence: true
     validates_uniqueness_of :target, scope: :requestor, message: 'Subcription already exists'
+
+    validates_email_format_of :requestor, :message => 'is not a valid email address'
+    validates_email_format_of :target, :message => 'is not a valid email address'
+
+    validate :target_should_not_be_equal_with_requestor
+    
+    def target_should_not_be_equal_with_requestor
+        errors.add(:target, 'Target should not be the same as requestor') if target == requestor
+    end
 end
