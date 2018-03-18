c8427e6e5fb9159ddec27c71a3b730a2b65cfd50
Phil Lacerna <phyllac@yahoo.com>
2018-03-19 00:12:48 +0800

    controller and test added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'connections/add_connection'
+  post 'connections/get_friends_list'
+  post 'connections/get_common_friends_list'
+  post 'connections/add_subscription'
+  post 'connections/block_user'
+  post 'connections/get_recipients'
 end
---
+++ spec/controllers/connections_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe ConnectionsController, type: :controller do
+
+    describe 'POST /connections/add_connection' do
+        before { post 'add_connection' }
+
+        it 'returns status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+
+    describe 'POST /connections/get_friends_list' do
+        before { post 'get_friends_list' }
+    
+        it 'returns status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+
+    describe 'POST /connections/get_common_friends_list' do
+        before { post 'get_common_friends_list' }
+    
+        it 'returns status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+
+    describe 'POST /connections/add_subscription' do
+        before { post 'add_subscription' }
+    
+        it 'returns status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+
+    describe 'POST /connections/block_user' do
+        before { post 'block_user' }
+    
+        it 'returns status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+
+    describe 'POST /connections/get_recipients' do
+        before { post 'get_recipients' }
+    
+        it 'returns status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+
+end
