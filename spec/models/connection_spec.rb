bfac2299aeef47ed2a50f4b7042a6acf0e9b1893
Phil Lacerna <phyllac@yahoo.com>
2018-03-19 21:17:07 +0800

    Added email validation on test environment.

--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
+require "validates_email_format_of/rspec_matcher"
 
 RSpec.describe Connection, type: :model do
   it { should validate_presence_of(:requestor) }
   it { should validate_presence_of(:target) }
+  it { should validate_email_format_of(:requestor).with_message('is not a valid email address') }
+  it { should validate_email_format_of(:target).with_message('is not a valid email address') }
   it { should validate_uniqueness_of(:target).scoped_to(:requestor).with_message('Connection already exists') }
   
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
+require "validates_email_format_of/rspec_matcher"
 
 RSpec.describe Subscription, type: :model do
   it { should validate_presence_of(:requestor) }
   it { should validate_presence_of(:target) }
+  it { should validate_email_format_of(:requestor).with_message('is not a valid email address') }
+  it { should validate_email_format_of(:target).with_message('is not a valid email address') }
   it { should validate_uniqueness_of(:target).scoped_to(:requestor).with_message('Subcription already exists') }
 end
